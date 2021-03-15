e6d4527478b6834840ec0550b83f63cf8b2cd26b
Ryan Kwast <15rek@queensu.ca>
2021-03-15 15:27:35 -0400

    Fixed Errors

--- run/first-sim.py
+++ run/first-sim.py
@@ -1,4 +1,4 @@
-from utilities.utilities import time_steps, number_of_nodes, memory
+from utilities.utilities import time_steps, memory
 from utilities.plots import draw_graph, plot_ave_infection_rate, plot_ave_red_proportion, total_infected_plot, average_total_infected_plot
 from model.model import network
 import matplotlib.pyplot as plt
@@ -21,12 +21,14 @@ for k in range(1):
     for j in range(time_steps):
         print("Urn " + str(G_super.nodes[0].id) + ". db: " + str(G_super.nodes[0].delta_b) + ", dr: " + str(G_super.nodes[0].delta_r) + "\n Red/black balls are " + str(G_super.nodes[0].total_red) +"/"+ str(G_super.nodes[0].total_black))
         
-        if j % 5 == 0:
-            draw_graph(G_super, j, k)
+        # if j % 5 == 0:
+        #     draw_graph(G_super, j, k)
+        
         G_super.supernode_run_step()
-        for i in G_super.nodes:
-            red_prop_data[j+1].append(i.red_proportion())
-            super_red_prop_data[j+1].append(i.super_red_proportion())
+        for node in G_super.nodes:
+            if node.alive == True:
+                red_prop_data[j+1].append(node.red_proportion())
+                super_red_prop_data[j+1].append(node.super_red_proportion())
 
         # time = []
         # average_red_prop = []
